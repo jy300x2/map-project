945c8b263533f5f1a75586fe3caa213f5cff4916
Jon Yu <jy300x2@gmail.com>
2016-07-28 14:57:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapProject::Application.config.secret_key_base = '50625bcf81a613aeacac463c48cdbb872cd1aaf3024c2766c633d316aa46c93c2e4b9ac3ded26f4507fc36602d312be2e942eb4e20cd3567fc48ba490f80e317'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MapProject::Application.config.session_store :cookie_store, key: '_map-project_session'
